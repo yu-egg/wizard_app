965bb8346ef18d871aac5ad8d54215c68f159e66
yu-egg <69953750+yu-egg@users.noreply.github.com>
2020-10-08 14:47:26 +0900

    ルーティングを編集しましょう まず、住所情報を登録させるページを表示するnew_addressアクションと住所情報を登録するcreate_addressアクションのルーティングを設定しましょう。

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,10 @@ Rails.application.routes.draw do
   devise_for :users, controllers: {
     registrations: 'users/registrations'
   }
+  devise_scope :user do
+    get 'addresses', to: 'users/registrations#new_address'
+    post 'addresses', to: 'users/registrations#create_address'
+  end
   root to: "home#index"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
